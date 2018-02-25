b6de42ed302d8392fa14736173ac6fa9b906fcc5
insanetron75 <timmy_hasler@hotmail.com>
2018-02-25 19:06:39 +0000

    added ui

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,18 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def convert
+    amount = params[:amount]
+    date = params[:date]
+    base_cur = params[:base_cur]
+    con_cur = params[:con_cur]
+
+    rate = Rate.at(date, base_cur, con_cur)
+    converted_amount = (amount.to_i * rate).round(2)
+    rate = rate.round(4)
+
+    @message = "#{amount} #{base_cur} = #{converted_amount} #{con_cur} @ #{rate}"
+
+  end
 end
